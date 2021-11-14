97eb8005db8ce36c1e507dd7491d3f98e0adaad9
Maria Sori <mariasori.school@gmail.com>
2021-11-13 22:52:02 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+  INSERT INTO voters (first_name, last_name, email)
+VALUES
+
+first_name, last_name, title, department, salary, manager
+
+John, Doe, Sales Lead, Sales, 100000, Ashley Rodriguez
+Mike, Chan, Salesperson, Sales, 80000, John Doe
+Ashley, Rodriguez, Lead Engineer, Engineering, 150000, null
+Kevin, Tupik, Software Engineer, Engineering, 120000, Ashley Rodriguez
+Malia, Brown, Accountant, Finance, 125000, null
+Sarah, Lourd, Legal Team Lead, Legal, 250000, null
+Tom, Allen, Lawyer, Legal, 190000, Sara Lourd
+Christian, Eckenrode, Lead Engineer, Engineering, 150000, Mike Chan
---
+++ server.js
@@ -0,0 +1,8 @@
+
+View All Employees
+View All Employees By Department
+View All Employees By Manager 
+Add Employee
+Remove Employee
+Update Employee Role 
+Update Employee Manager
